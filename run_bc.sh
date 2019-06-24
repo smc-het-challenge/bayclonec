c20d1520b1eda703ee8ea3a4f73f4ab7ed71610a
compgenome365 <compgenome365@gmail.com>
2019-06-24 16:37:53 -0500

    added the path for the script

--- run_bc.sh
+++ run_bc.sh
@@ -2,14 +2,17 @@
 ### example run in two steps
 ### intermediate file generation for Rscript
 
+### get the absolute path
+SPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 ### ./run_bc.sh P1-noXY.mutect.vcf P1-noXY.battenberg.txt P1-noXY.cellularity_ploidy.txt P1
 if [ $# != 4 ]; then
     echo "Please enter: ./run_bc.sh <mutect vcf file> <battenberg file> <cellularity_ploidy file> <out_prefix>"
 	exit
 else
     echo "running codes ... "
-	./parseInputData_smc $1 $2 temp.out
+	$SPATH/parseInputData_smc $1 $2 temp.out
 	### Rscript run
-	Rscript BayCloneC_smc.R temp.out $3 $4 > run1.log 2>&1
+	Rscript $SPATH/BayCloneC_smc.R temp.out $3 $4 > run1.log 2>&1
 fi
 
